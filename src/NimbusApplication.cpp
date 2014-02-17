d4fe5b1d08b97945deaa193c7341155fad191233
leyarotheconquerer <HazenJohnson@letu.edu>
2014-02-17 00:08:56 -0600

    Stubbed a basic framework for the NimbusApplication.
    One weakness of this arrangement is that it aggregates a lot of
    functionality in a few functions. However, this makes tracing the line of
    execution along the base framework more simple, so I think it's a good
    idea.
    
    As we develop more of the framework, we should consider decentralizing a
    lot of the initialization stuff that this base class does. (e.g. scene
    creation should be delegated to run mode and OIS input handling should be
    handled by the input manager.)

---
+++ src/NimbusApplication.cpp
@@ -0,0 +1,23 @@
+#include "NimbusApplication.h"
+
+NimbusApplication::NimbusApplication(void)
+{
+}
+
+NimbusApplication::~NimbusApplication(void)
+{
+}
+
+void NimbusApplication::begin(void)
+{
+}
+
+bool NimbusApplication::loadConfiguration(void)
+{
+	return true;
+}
+
+bool NimbusApplication::createScene(void)
+{
+	return true;
+}
---
+++ src/NimbusApplication.h
@@ -0,0 +1,70 @@
+#ifndef NIMBUSAPPLICATION_H
+#define NIMBUSAPPLICATION_H
+
+#include <OgreRoot.h>
+
+using namespace Ogre;
+
+/** The driving application base for Nimbus.
+
+This application current handles much of the interface with Ogre.
+Eventually, a lot of this should eventually be delegated to appropriate
+subclasses. (e.g. input management should be given to InputManager)
+ 
+One possible consideration we should take is making a main loop that drives
+the application independent from Ogre. This would make our game more
+independent rather than depending on Ogre for it's vital running
+functionality.
+*/
+class NimbusApplication
+{
+private:
+	// The singleton variable (this application overuses the Singleton design)
+	static NimbusApplication app;
+
+	// Member Variables
+
+	// The Ogre::Root object for this application
+	Root* mRoot;
+	// The Ogre::RenderWindow for this application
+	RenderWindow* mWindow;
+
+	// Member Functions
+
+	/** Loads all configuration files for the application.
+	 
+	This function creates the Ogre::Root object, initializes the resource
+	groups, and sets up the RenderSystem.
+	 
+	@return Bool indicating success of loading.
+	*/
+	bool loadConfiguration(void);
+
+	/** A test function which will be used to show that Ogre has been loaded
+	properly. Requires that the mRoot has been initialized.
+	
+	DO NOT use this for final application. Instead, create the SceneManager
+	in the appropriate RunMode. Each RunMode should probably have its own
+	SceneManager and scene construction method.
+	
+	@return Bool indicating success of creating the scene.
+	*/
+	bool createScene(void);
+
+	/** Private default constructor to prevent instances of the
+	NimbusApplication class.
+	*/
+	NimbusApplication(void);
+
+public:
+	virtual ~NimbusApplication(void);
+
+	/** Starts the application running.
+	
+	The application will continue running according to Ogre::FrameListener
+	triggers until internally terminated. (Specifically renderFrameQueued.)
+	*/
+	static void begin(void);
+};
+
+#endif
