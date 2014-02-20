17c717d39c7a75e5af3e1d0c5e6fc05eca14a62e
drac0nic <spenser.m.bray@gmail.com>
2014-02-19 21:02:51 -0600

    Implemented namespace and some dependencies for the Event framework
    Also updated the Nimbus project files.

---
+++ EventListener.h
@@ -0,0 +1,9 @@
+#ifndef NIMBUS_EVENTLISTENER_H
+#define NIMBUS_EVENTLISTENER_H
+
+namespace Nimbus 
+{
+	// CLASS INFORMATION
+}
+
+#endif
---
+++ EventSystem.h
@@ -0,0 +1,9 @@
+#ifndef NIMBUS_EVENTSYSTEM_H
+#define NIMBUS_EVENTSYSTEM_H
+
+namespace Nimbus
+{
+	// CLASS INFORMATION
+}
+
+#endif
