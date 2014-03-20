cb43ac45003a94d4af436475c60cf7e8eaf31b51
drac0nic <spenser.m.bray@gmail.com>
2014-03-19 21:20:39 -0700

    Comment and fix up Tile header.

--- src/Tile.h
+++ src/Tile.h
@@ -1,27 +1,92 @@
-#include "Corner.h"
+#ifndef NIMBUS_TILE_H
+#define NIMBUS_TILE_H
 
-//Temporary; fill or replace
-	enum Biome{MOUNTAIN, DESERT, COAST, OCEAN};
+#include <vector>
 
-//This is the Center class renamed to fit our terms.
-class Tile{
-public:
-	static int deltaX;
-	static int deltaY;
+#include <OGRE/Vector2.h>
 
-	int index;
-	Point loc;
-	std::vector<Corner*> corners;
-	std::vector<Tile*> neighbors;
-	std::vector<TileEdge*> borders;
-	bool border, ocean, water, coast;
-	double elevation;
-	double moisture;
-	double area;
-	Biome biome;
+namespace Nimbus
+{
+	//Temporary; fill or replace
+	enum Biome { MOUNTAIN, DESERT, COAST, OCEAN };
 
-	Tile();
-	Tile(Point *loc);
-	void calculateBox();
-	void toString();
-};
+	// Class forward-declarations! Whoo!
+	class Corner;
+	class TileEdge;
+
+	/* Typedef Vector2 to Point, to improve readability. */
+	typedef Ogre::Vector2 Point;
+
+	/* This holds the Tile class, originally named "Center." */
+	class Tile
+	{
+	public:
+		// CLASS VARIABLES --
+		/* The delta X of the Tile class. */
+		static int deltaX;
+
+		/* The delta Y of the Tile class. */
+		static int deltaY;
+
+		// MEMEBER VARIABLES --
+		/* The index of this tile. */
+		int index;
+
+		/* The location of this tile. */
+		Point loc;
+
+		/* A vector containing all the corners defining the tile. */
+		std::vector<Corner*> corners;
+
+		/* A vector containing all the neighboring tiles. */
+		std::vector<Tile*> neighbors;
+
+		/* A vector containing the borders of the tile. */
+		std::vector<TileEdge*> borders;
+
+		/* Whether or not this is a border tile. */
+		bool border;
+
+		/* Holds whether not this tile is an ocean. */
+		bool ocean;
+
+		/* Holds whether not this tile is a water tile. */
+		bool water;
+
+		/* Holds whether not this tile is a coast. */
+		bool coast;
+
+		/* Holds this tile's elevation. */
+		double elevation;
+
+		/* Holds this tile's moisture level. */
+		double moisture;
+
+		/* Holds this tile's area. */
+		double area;
+
+		/* Holds this tile's biome. */
+		Biome biome;
+
+		// CONSTRUCTORS --
+		/* Blank constructor! */
+		Tile(void);
+
+		/* Constructor given the tile's location.
+		  @param loc The location of the Tile.
+		*/
+		Tile(Point *loc);
+
+		/* Destructor! */
+		virtual ~Tile(void);
+		
+		// MEMBER FUNCTIONS --
+		/* Calculate box of the tile. */
+		void calculateBox(void);
+
+		/* Convert the tile to string format! */
+		void toString(void);
+	};
+}
+
+#endif
