8996e34262821c0d3a8c49ecd8eb1ef7c6192b01
drac0nic <spenser.m.bray@gmail.com>
2014-03-20 13:37:18 -0500

    Cleanup and resolve dependencies of TileEdge.

--- src/TileEdge.cpp
+++ src/TileEdge.cpp
@@ -1,14 +1,18 @@
-#include "Map.h"
+#include "TileEdge.h"
+#include "Corner.h"
 
 using namespace Nimbus;
 
-void TileEdge::setVoronoi(Corner *c0, Corner *c1){
-	v0 = c0;
-	v1 = c1;
+void TileEdge::setVoronoi(Corner* c0, Corner* c1)
+{
+	Corner* v0 = c0;
+	Corner* v1 = c1;
+	
 	midpoint = new Point((v0->loc->x + v1 ->loc->x) / 2, (v0->loc->y + v1 ->loc->y) / 2);
 }
 
-void TileEdge::toString(){
+void TileEdge::toString(void)
+{
 	std::cout << "TileEdge:\n";
 	std::cout << "c1, x:" << v0->loc->x << ", y:" << v0->loc->y  << "\n";
 	std::cout << "c2, x:" << v1->loc->x << ", y:" << v1->loc->y  << "\n";
