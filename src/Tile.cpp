90c36f39ebec2ffe7a16e11d04bf677f96f8359e
drac0nic <spenser.m.bray@gmail.com>
2014-03-20 13:36:55 -0500

    Cleanup and resolve dependencies of Tile.

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,18 +1,22 @@
 #include "Tile.h"
+#include "Corner.h"
 
 using namespace Nimbus;
 
 int Tile::deltaX = 0;
 int Tile::deltaY = 0;
 
-Tile::Tile(){
+Tile::Tile()
+{
 }
 
-Tile::Tile(Point *loc){
-	Tile::loc = *loc;
+Tile::Tile(Point *loc)
+{
+	this->loc = *loc;
 }
 
-void Tile::calculateBox(){
+void Tile::calculateBox(void)
+{
 	for (int i = 0; i < corners.size(); i++){
 		double x = std::abs(corners.at(i)->loc->x - loc.x);
 		if(x > deltaX){
@@ -26,7 +30,7 @@ void Tile::calculateBox(){
 	}
 }
 
-void Tile::toString(){
+void Tile::toString(void)
+{
 	std::cout << "Tile, x:" << loc.x << ", y:" << loc.y << " " << (border? "b": "-") << (ocean? "o": "-") << (water? "w": "-") << (coast? "c": "-") << "\n";
-
 }
