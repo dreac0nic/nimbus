1d06360b756fc6e6fc7f03016e294c7196f698d1
RemingtonWells <kaospyke@att.net>
2014-03-24 13:56:16 -0600

    fossil of tile by Remington

---
+++ src/Tile.cpp
@@ -0,0 +1,77 @@
+using namespace Nimbus
+
+	public class Tile
+
+main ()
+{
+	private int biome;
+	private double humidity;
+	private double temp;
+	private double grndSat;
+	private int elevation;
+	private bool burning;
+	private double grndSatLmt;
+	private int tileId;
+	// to include: neighbor memory of some kind
+
+
+	// create function with inputs for humidity, temperature, and ground saturation respectively
+	public tile(double humid, double temper, double grnd)
+	{
+
+	}
+
+
+
+	public double getHumidity()
+	{
+		return humidity;
+	}
+
+	public double getTemp()
+	{
+		return temp;
+	}
+
+	public double getSat()
+	{
+		return grndSat;
+	}
+
+	private void setHumidity()
+	{
+		
+	}
+
+	private void setTemp()
+	{
+
+	}
+
+	private void setSat()
+	{
+
+	}
+
+	public void updateTile()
+	{
+
+	}
+
+	private void setType()
+	{
+
+	}
+
+	public String getType()
+	{
+
+		return "words";
+	}
+
+	private void spawn()
+	{
+
+	}
+
+}
