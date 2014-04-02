559f79226d4308785c029dda63994addf1700378
drac0nic <spenser.m.bray@gmail.com>
2014-04-02 01:31:57 -0500

    Add test tile for testing.

--- src/TestMode.cpp
+++ src/TestMode.cpp
@@ -94,6 +94,47 @@ bool TestMode::initialize()
 	light = mSceneMgr->createLight("MainLight");
 	light->setPosition(20, 80, 50);
 
+	// Generate some fake map information and throw it into the scene!
+	const int pointCount = 6;
+	const float lPI = 3.14159f;
+	const float tileScalar = 10.0f;
+	Tile testTile;
+
+	Corner* corn1 = new Corner();
+	Corner* corn2 = new Corner();
+	Corner* corn3 = new Corner();
+	Corner* corn4 = new Corner();
+	Corner* corn5 = new Corner();
+
+	corn1->loc = new Ogre::Vector2(0.0f, 1.0f);
+	corn2->loc = new Ogre::Vector2(-1.0f, 1.0f);
+	corn3->loc = new Ogre::Vector2(-1.0f, 0.0f);
+	corn4->loc = new Ogre::Vector2(0.0f, -1.0f);
+	corn5->loc = new Ogre::Vector2(1.0f, -1.0f);
+
+	testTile.corners.push_back(corn1);
+	testTile.corners.push_back(corn2);
+	testTile.corners.push_back(corn3);
+	testTile.corners.push_back(corn4);
+	testTile.corners.push_back(corn5);
+
+	/*
+	for(int i = 0; i < pointCount; ++i) {
+		Corner* tempCorner = new Corner();
+		tempCorner->loc = new Ogre::Vector2(tileScalar*cos(2*lPI*(i/pointCount)), tileScalar*sin(2*lPI*(i/pointCount)));
+
+		testTile.corners.push_back(tempCorner);
+	}
+	*/
+
+	Entity* tileEntity = mSceneMgr->createEntity("testTile", testTile.getMesh());
+	SceneNode* tileNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+
+	tileNode->attachObject(tileEntity);
+
+	tileNode->setPosition(0, 0, -150);
+	tileNode->setScale(40.0f, 40.0f, 40.0f);
+
 	// Note that the RunMode has been initialized
 	this->initialized = true;
 	return true;
