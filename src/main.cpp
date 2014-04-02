ceabcc188fbd300ed0c4f17ba9fb3256af6ede24
drac0nic <spenser.m.bray@gmail.com>
2014-04-02 01:31:13 -0500

    Add appropriate application launch code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-#include "Map.h"
-#include "MapGeneration\Voronoi.h"
 
-using namespace Nimbus::Voronoi;
+#include "NimbusApplication.h"
 
-void pause(){
-	std::cout << "Press Enter.\n";
-	std::cin.get();
-}
-
-void testVoronoi(){
-	Edge::initList(0,1,2);
-	Edge::initQueue();
-	Halfedge::initQueue(0, 1, 2);
-	Site site1 = Site(new Point(0,0), 0, 1.0);
-	Site site2 = Site(new Point(0,4), 1, 3.0);
-
-	Edge edge = *Edge::createBisectingEdge(&site1, &site2);
-
-	edge.clipVertices(new Rectangle(-1,-1,2,2));
-
-	pause();
-}
+using namespace Nimbus;
 
 /** The main function to start the Nimbus application.
 */
 int main(int argc, char* argv[])
 {
-	double width = 600;
-	double height = 600;
-	int numsites = 3000;
-	std:: vector<Point*> points;
-	srand(94870395729);
+	// Initialize the application
+	NimbusApplication* application = new NimbusApplication();
 
-	Voronoi v = Voronoi(numsites, width, height);
+	// This used to be a placeholder for a render window so I knew the application ran...
+	// Now I just like the dragon... :)
+	std::cout << "Nimbus Application... in development" << std::endl << std::endl;
+	std::cout << "Meanwhile, here is a dragon:" << std::endl;
+	std::cout << "\n                                                 /===-_---~~~~~~~~~------____\n                                                |===-~___                _,-\'\n                 -==\\\\                         `//~\\\\   ~~~~`---.___.-~~\n             ______-==|                         | |  \\\\           _-~`\n       __--~~~  ,-/-==\\\\                        | |   `\\        ,\'\n    _-~       /\'    |  \\\\                      / /      \\      /\n  .\'        /       |   \\\\                   /\' /        \\   /\'\n /  ____  /         |    \\`\\.__/-~~ ~ \\ _ _/\'  /          \\/\'\n/-\'~    ~~~~~---__  |     ~-/~         ( )   /\'        _--~`\n                  \\_|      /        _)   ;  ),   __--~~\n                    \'~~--_/      _-~/-  / \\   \'-~ \\\n                   {\\__--_/}    / \\\\_>- )<__\\      \\\n                   /\'   (_/  _-~  | |__>--<__|      | \n                  |0  0 _/) )-~     | |__>--<__|      |\n                  / /~ ,_/       / /__>---<__/      |  \n                 o o _//        /-~_>---<__-~      /\n                 (^(~          /~_>---<__-      _-~              \n                ,/|           /__>--<__/     _-~                 \n             ,//(\'(          |__>--<__|     /                  .----_ \n            ( ( \'))          |__>--<__|    |                 /\' _---_~\\\n         `-)) )) (           |__>--<__|    |               /\'  /     ~\\`\\\n        ,/,\'//( (             \\__>--<__\\    \\            /\'  //        ||\n      ,( ( ((, ))              ~-__>--<_~-_  ~--____---~\' _/\'/        /\'\n    `~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/ \n  ._-~//( )/ )) `                    ~~-\'_/_/ /~~~~~~~__--~ \n   ;\'( \')/ ,)(                              ~~~~~~~~~~ \n  \' \') \'( (/\n    \'   \'  `   ";
+	std::cout << std::endl;
+
+	// Begin the application
+	NimbusApplication::begin();
+
+	// Pause the application
+	//std::cin.get();
+
+	// Cleanup!
+	delete application;
 }
