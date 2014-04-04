42a2e1d5a3b0b11f89faf9572407b806dade4e7f
leyarotheconquerer <HazenJohnson@letu.edu>
2014-04-04 17:20:04 -0500

    Specify how WindMap should work using the new paradigm.

--- src/WindMap.h
+++ src/WindMap.h
@@ -16,40 +16,65 @@ namespace Nimbus
 	class WindMap
 	{
 	private:
-		// Stores two dimensional array of Vector2s
-		VectorMap* vectorMap;
+		/** The resolution of the vector map grid. */
+		Ogre::Real mResolution;
+
+		/** The vector grid to represent wind strength and direction. */
+		Grid<Ogre::Vector2>* mVectorMap;
+
+		/** The list of wind currents which currently affect the wind map. */
+		std::list<WindCurrent> mCurrents;
 		
 	public:
-		WindMap(int mapSizeX, int mapSizeY);
+		/** Creates a basic wind map.
+			@param resolution The resolution of the vector map in terms of world coordinates.
+				This value is the world distance between each wind vector in the vector map.
+		*/
+		WindMap(Ogre::Real resolution);
 		virtual ~WindMap(void);
 
-		// Set up VectorMap
-		void Initialize(void);
+		/** Recalculates the vector map based on the current list of wind currents.
+		*/
+		void update(void);
 
-		// Stores list of WindCurrents
-		std::list<WindCurrent> currents;
+		/** Adds a wind current to the wind current list.
+			@param windCurrent A pointer to the wind current which should be added to the wind map.
+		*/
+		void addWindCurrent(WindCurrent* windCurrent);
 
-		// Stores size of vectorMap
-		int sizeX;
-		int sizeY;
+		/** Removes a wind current from the current wind current list
+			@param windCurrent A pointer to the wind current which should be deleted from the wind map.
+		*/
+		void removeWindCurrent(WindCurrent* windCurrent);
 
-		// Gets Vector2 at position, will find average vector if given non-integer numbers
-		Ogre::Vector2 getVector(double x, double y);
+		/** Gets the wind vector at the given map coordinates
+			@param x The x coordinate in map coordinates of the wind vector to retrieve.
+			@param y The y coordinate in map coordinates of the wind vector to retrieve.
+		*/
+		Ogre::Vector2 getWindVector(Ogre::Real x, Ogre::Real y);
 
-		// Gets Vector2 at Vector2, will find average vector if given non-integer numbers
-		Ogre::Vector2 getVector(Ogre::Vector2 position);
+		/** Gets the wind vector at the given map coordinates.
+			@param position The x and y coordinates in map coordinates of the wind vector to retrieve.
+		*/
+		Ogre::Vector2 getWindVector(Ogre::Vector2 position);
 
-		// Gets average Vector2 in a square area
-		Ogre::Vector2 getAreaAverage(double topLeftx, double topLefty, double botRightx, double botRighty);
-		Ogre::Vector2 getAreaAverage(Ogre::Vector2 topLeft, Ogre::Vector2 botRight);
+		/** Gets the average wind vector in the given area of map coordinates.
+			@param topLeftx The top left x coordinate in map coordinates
+			@param topLefty The top left y coordinate in map coordinates
+			@param botRightx The bottom right x coordinate in map coordinates
+			@param botRighty The bottom right y coordinate in map coordinates
+		*/
+		Ogre::Vector2 getAverageWindVector(Ogre::Real topLeftx, Ogre::Real topLefty, Ogre::Real botRightx, Ogre::Real botRighty);
 
-		// Sets Vector2 at position to strength
-		void setVector(double posx, double posy, double strx, double stry);
+		/** Gets the average wind vector in the given area of map coordinates.
+			@param topLeft The top left corner of the area in map coordinates
+			@param botRight The bottom right corner of the area in map coordinates
+		*/
+		Ogre::Vector2 getAverageWindVector(Ogre::Vector2 topLeft, Ogre::Vector2 botRight);
 
-		// Sets Vector2 at Vector2 to vector
-		void setVector(Ogre::Vector2 position, Ogre::Vector2 strength);
-
-		void updateArrows();
+		/** Gets the current list of wind currents.
+		*/
+		std::list<WindCurrent>* getWindCurrents();
 	};
 }
 #endif
