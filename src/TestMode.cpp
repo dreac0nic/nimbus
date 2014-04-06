4f9538589d43ae1689f9caaac8ea2f5498e02682
Samus1111111 <Samus1111111@hotmail.com>
2014-04-06 09:40:42 -0500

    Created a new panel and arranged the panels so the words are in the correct location for the button.

--- src/TestMode.cpp
+++ src/TestMode.cpp
@@ -13,7 +13,6 @@
 #include <OgreFont.h>
 #include <OgreFontManager.h>
 
-
 using namespace Nimbus;
 using namespace Ogre;
 
@@ -57,18 +56,18 @@ Light* light;
 mSceneMgr = Root::getSingleton().createSceneManager("DefaultSceneManager");
 
 // Overlay stuffz
-Ogre::OverlaySystem* overlaySystem = new Ogre::OverlaySystem;
+OverlaySystem* overlaySystem = new OverlaySystem;
 mSceneMgr->addRenderQueueListener(overlaySystem);
 
-Ogre::OverlayManager* mOverlayMgr = Ogre::OverlayManager::getSingletonPtr();
+OverlayManager* mOverlayMgr = OverlayManager::getSingletonPtr();
 
-Ogre::Overlay* theOverlay = mOverlayMgr->create("theOverlay");
+Overlay* theOverlay = mOverlayMgr->create("theOverlay");
 
 theOverlay->setZOrder(500);
 
 // Font
-Ogre::FontPtr mFont = Ogre::FontManager::getSingleton().create("Font", "General");
-mFont->setType(Ogre::FT_TRUETYPE);
+FontPtr mFont = FontManager::getSingleton().create("Font", "General");
+mFont->setType(FT_TRUETYPE);
 mFont->setSource("../../times.ttf");
 mFont->setTrueTypeSize(32);
 mFont->setTrueTypeResolution(96);
@@ -76,30 +75,69 @@ mFont->addCodePointRange(Ogre::Font::CodePointRange(33, 255));
 
 
 // Overlay element
-Ogre::OverlayContainer* mPanel = static_cast<Ogre::OverlayContainer*>(mOverlayMgr->createOverlayElement("Panel", "PANEL"));
-mPanel->setMetricsMode(Ogre::GMM_RELATIVE);
-mPanel->setPosition(0.25, 0.25);						// This sets the position of the menu on the screen
-mPanel->setDimensions(0.5, 0.5);						// This sets the width and height of the menu on the screen
+OverlayContainer* mPanel = static_cast<OverlayContainer*>(mOverlayMgr->createOverlayElement("Panel", "PANEL"));
+mPanel->setMetricsMode(GMM_RELATIVE);
+mPanel->setPosition(0.35, 0.25);							// This sets the position of the menu on the screen
+mPanel->setDimensions(0.3, 0.5);						// This sets the width and height of the menu on the screen
 mPanel->setMaterialName( "BaseWhite" );					// This makes the menu color white
 
+// Overlay buttons
+OverlayContainer* resumeButton = static_cast<OverlayContainer*>(mOverlayMgr->createOverlayElement("Panel", "RESUMEBUTTON"));
+resumeButton->setMetricsMode(GMM_RELATIVE);
+resumeButton->setPosition(0.47, 0.3);
+resumeButton->setDimensions(0.0, 0.0);
+//resumeButton->setMaterialName("BaseWhite");
+
+OverlayContainer* quitButton = static_cast<OverlayContainer*>(mOverlayMgr->createOverlayElement("Panel", "QUITBUTTON"));
+quitButton->setMetricsMode(GMM_RELATIVE);
+quitButton->setPosition(0.48, 0.5);
+quitButton->setDimensions(0.0, 0.0);
+//quitButton->setMaterialName("BaseWhite");
+
+
 // Overlay element text
-Ogre::TextAreaOverlayElement* mTextArea = static_cast<Ogre::TextAreaOverlayElement*>(mOverlayMgr->createOverlayElement("TextArea", "TEXT"));
-mTextArea->setFontName("Font");
-mTextArea->setCharHeight(16);
-mTextArea->setColour(Ogre::ColourValue(0.9, 0.9, 0.9));
-printf("%s ",mTextArea->getName());
-mTextArea->setCaption("HELLO!!!!");
+TextAreaOverlayElement* resume = static_cast<TextAreaOverlayElement*>(mOverlayMgr->createOverlayElement("TextArea", "RESUME"));
+resume->setMetricsMode(GMM_PIXELS);
+resume->setFontName("Font");
+resume->setPosition(0.0, 0.0);
+resume->setDimensions(0.1, 0.1);
+resume->setCaption("Resume");
+resume->setCharHeight(16);
+//resume->setColourBottom(ColourValue(1.0, 0.0, 0.0));
+//resume->setColourTop(ColourValue(0.0, 1.0, 0.0));
+resume->setColour(ColourValue(0.0, 1.0, 0.0));
+printf("resume name = %s\n",resume->getName());
 
 
-mPanel->addChild(mTextArea);
+TextAreaOverlayElement* quit = static_cast<TextAreaOverlayElement*>(mOverlayMgr->createOverlayElement("TextArea", "QUIT"));
+quit->setMetricsMode(GMM_PIXELS);
+quit->setFontName("Font");
+quit->setPosition(0.0, 0.0);
+quit->setDimensions(0.1, 0.1);
+quit->setCaption("Quit");
+quit->setCharHeight(16);
+//quit->setColourBottom(ColourValue(1.0, 0.0, 0.0));
+//quit->setColourTop(ColourValue(0.0, 1.0, 0.0));
+quit->setColour(ColourValue(1.0, 0.0, 0.0));
+printf("quit name = %s\n",quit->getName());
+
 theOverlay->add2D(mPanel);
+theOverlay->add2D(resumeButton);
+theOverlay->add2D(quitButton);
+resumeButton->addChild(resume);
+quitButton->addChild(quit);
 
-mTextArea->show();
-mPanel->show();
 theOverlay->show();
 
+// testing
+printf("Overlay Enabled = %d\n", theOverlay->isInitialised());
+printf("mPanel Enabled = %d\n", mPanel->isEnabled());
+printf("resumeButton Enabled = %d\n", resumeButton->isEnabled());
+printf("resume Enabled = %d\n", resume->isEnabled());
+printf("quit Enabled = %d\n", quit->isEnabled());
+printf("resume Visible = %d\n", resume->isVisible());
+printf("quit Visible = %d\n", quit->isVisible());
 
-printf("Overlay Enabled?? = %d", theOverlay->isInitialised()); 
 // Create the camera
 mCamera = mSceneMgr->createCamera("PlayerCam");
 
