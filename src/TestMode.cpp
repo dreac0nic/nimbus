758bad79ae5a44b8cba300eae01d9dc486503b1a
drac0nic <spenser.m.bray@gmail.com>
2014-04-10 13:07:35 -0500

    Remove test cube code because it is dumb.

--- src/TestMode.cpp
+++ src/TestMode.cpp
@@ -152,138 +152,16 @@ bool TestMode::initialize()
 	material->getTechnique(0)->getPass(0)->setVertexColourTracking(TVC_AMBIENT);
 
 	// Create entity and add to the scene.
-	Entity* tileEntity = mSceneMgr->createEntity("testTile", testTile.getMesh());
+	Entity* tileEntity = mSceneMgr->createEntity("testTile", testTile.getMesh("tileMesh"));
 	SceneNode* tileNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-
-	std::cout << "Blah!" << std::endl;
 
 	tileEntity->setMaterialName("Tiles/Default");
 
 	tileNode->attachObject(tileEntity);
 
-	tileNode->setPosition(0, 0, -50);
+	tileNode->setPosition(0, 0, -80);
 	tileNode->setScale(10.0f, 10.0f, 10.0f);
 
-	// BLARG TESTING
-	    /// Create the mesh via the MeshManager
-    Ogre::MeshPtr msh = MeshManager::getSingleton().createManual("ColourCube", "General");
- 
-    /// Create one submesh
-    SubMesh* sub = msh->createSubMesh();
- 
-    const float sqrt13 = 0.577350269f; /* sqrt(1/3) */
- 
-    /// Define the vertices (8 vertices, each consisting of 2 groups of 3 floats
-    const size_t nVertices = 6;
-    const size_t vbufCount = 3*2*nVertices;
-    float vertices[vbufCount] = {
-    		testTile.loc.x, 0.0f, testTile.loc.y,
-    		0.0f,1.0f,0.0f,
-            corn1->loc->x,0.0f,corn1->loc->y,        //0 position
-            0.0f,1.0f,0.0f,     //0 normal
-            corn2->loc->x,0.0f,corn2->loc->y,         //1 position
-            0.0f,1.0f,0.0f,      //1 normal
-            corn3->loc->x,0.0f,corn3->loc->y,        //2 position
-            0.0f,1.0f,0.0f,     //2 normal
-            corn4->loc->x,0.0f,corn4->loc->y,       //3 position
-            0.0f,1.0f,0.0f,    //3 normal
-            corn5->loc->x,0.0f,corn5->loc->y,         //4 position
-            0.0f,1.0f,0.0f,      //4 normal
-    };
- 
-    RenderSystem* rs = Root::getSingleton().getRenderSystem();
-    RGBA colours[nVertices];
-    RGBA *pColour = colours;
-    // Use render system to convert colour value since colour packing varies
-    rs->convertColourValue(ColourValue(1.0,0.0,0.0), pColour++); //0 colour
-    rs->convertColourValue(ColourValue(1.0,1.0,0.0), pColour++); //1 colour
-    rs->convertColourValue(ColourValue(0.0,1.0,0.0), pColour++); //2 colour
-    rs->convertColourValue(ColourValue(0.0,0.0,0.0), pColour++); //3 colour
-    rs->convertColourValue(ColourValue(1.0,0.0,1.0), pColour++); //4 colour
-    rs->convertColourValue(ColourValue(0.0,1.0,1.0), pColour++); //4 colour
- 
-    /// Define 12 triangles (two triangles per cube face)
-    /// The values in this table refer to vertices in the above table
-    const size_t ibufCount = 15;
-    unsigned short faces[ibufCount] = {
-            0,2,1,
-            0,3,2,
-            0,4,3,
-            0,5,4,
-            0,1,5
-    };
- 
-    /// Create vertex data structure for 8 vertices shared between submeshes
-    msh->sharedVertexData = new VertexData();
-    msh->sharedVertexData->vertexCount = nVertices;
- 
-    /// Create declaration (memory format) of vertex data
-    VertexDeclaration* decl = msh->sharedVertexData->vertexDeclaration;
-    size_t offset = 0;
-    // 1st buffer
-    decl->addElement(0, offset, VET_FLOAT3, VES_POSITION);
-    offset += VertexElement::getTypeSize(VET_FLOAT3);
-    decl->addElement(0, offset, VET_FLOAT3, VES_NORMAL);
-    offset += VertexElement::getTypeSize(VET_FLOAT3);
-    /// Allocate vertex buffer of the requested number of vertices (vertexCount) 
-    /// and bytes per vertex (offset)
-    HardwareVertexBufferSharedPtr vbuf = 
-        HardwareBufferManager::getSingleton().createVertexBuffer(
-        offset, msh->sharedVertexData->vertexCount, HardwareBuffer::HBU_STATIC_WRITE_ONLY);
-    /// Upload the vertex data to the card
-    vbuf->writeData(0, vbuf->getSizeInBytes(), vertices, true);
- 
-    /// Set vertex buffer binding so buffer 0 is bound to our vertex buffer
-    VertexBufferBinding* bind = msh->sharedVertexData->vertexBufferBinding; 
-    bind->setBinding(0, vbuf);
- 
-    // 2nd buffer
-    offset = 0;
-    decl->addElement(1, offset, VET_COLOUR, VES_DIFFUSE);
-    offset += VertexElement::getTypeSize(VET_COLOUR);
-    /// Allocate vertex buffer of the requested number of vertices (vertexCount) 
-    /// and bytes per vertex (offset)
-    vbuf = HardwareBufferManager::getSingleton().createVertexBuffer(
-        offset, msh->sharedVertexData->vertexCount, HardwareBuffer::HBU_STATIC_WRITE_ONLY);
-    /// Upload the vertex data to the card
-    vbuf->writeData(0, vbuf->getSizeInBytes(), colours, true);
- 
-    /// Set vertex buffer binding so buffer 1 is bound to our colour buffer
-    bind->setBinding(1, vbuf);
- 
-    /// Allocate index buffer of the requested number of vertices (ibufCount) 
-    HardwareIndexBufferSharedPtr ibuf = HardwareBufferManager::getSingleton().
-        createIndexBuffer(
-        HardwareIndexBuffer::IT_16BIT, 
-        ibufCount, 
-        HardwareBuffer::HBU_STATIC_WRITE_ONLY);
- 
-    /// Upload the index data to the card
-    ibuf->writeData(0, ibuf->getSizeInBytes(), faces, true);
- 
-    /// Set parameters of the submesh
-    sub->useSharedVertices = true;
-    sub->indexData->indexBuffer = ibuf;
-    sub->indexData->indexCount = ibufCount;
-    sub->indexData->indexStart = 0;
- 
-    /// Set bounding information (for culling)
-    msh->_setBounds(AxisAlignedBox(-100,-100,-100,100,100,100));
-    msh->_setBoundingSphereRadius(Math::Sqrt(3*100*100));
- 
-    /// Notify -Mesh object that it has been loaded
-    msh->load();
-
-    MaterialPtr materialThing = MaterialManager::getSingleton().create("Test/ColourTest", ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-	materialThing->getTechnique(0)->getPass(0)->setVertexColourTracking(TVC_AMBIENT);
-
-	Entity* thisEntity = mSceneMgr->createEntity("cc", "ColourCube");
-	thisEntity->setMaterialName("Test/ColourTest");
-	SceneNode* thisSceneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-	thisSceneNode->setPosition(0, 0, -100);
-	thisSceneNode->setScale(10.0f, 10.0f, 10.0f);
-	thisSceneNode->yaw(Ogre::Radian(90.0f));
-	thisSceneNode->attachObject(thisEntity);
 	// Target the tile for tracking.
 	this->mCamera->setAutoTracking(true, tileNode);
 
