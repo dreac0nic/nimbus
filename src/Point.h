934233a46843c794a30b9a3c1af2ac8401b1c250
Mark Smullen <mark@techfocus.net>
2014-04-12 18:59:05 -0500

    Add Ogre::Vector3s to points

--- src/Point.h
+++ src/Point.h
@@ -1,6 +1,8 @@
 #ifndef VORONOI_POINT_H
 #define VORONOI_POINT_H
 
+#include "OgreRoot.h"
+
 namespace Nimbus
 {
 	namespace Voronoi
@@ -8,11 +10,17 @@ namespace Nimbus
 		class Point
 		{
 		public:
-			Point(int x, int y) : x(x), y(y) { }
+			Point(int x, int y) : x(x), y(y) { point = Ogre::Vector3(x, 0.0F, y); }
 			~Point() { }
 
+		private:
+			Ogre::Vector3 point;
+
+		public:
 			int x;
 			int y;
+
+			Ogre::Vector3 getPoint(void) { return point; }
 		};
 	}
 }
